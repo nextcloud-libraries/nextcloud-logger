ddc68d06ffbad0b655f71a6520a0859a7d2fd98a
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-01-08 20:33:19 +0100

    Enhance generated documentation
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,17 @@
 import { buildConsoleLogger } from './ConsoleLogger'
 import { ILogger } from './contracts'
-import { LoggerBuilder } from './Builder'
+import { LoggerBuilder } from './LoggerBuilder'
 
+/**
+ * Build a customized logger instance
+ */
 export function getLoggerBuilder(): LoggerBuilder {
     return new LoggerBuilder(buildConsoleLogger)
 }
 
+/**
+ * Get a default logger instance without any configuration
+ */
 export function getLogger(): ILogger {
     return getLoggerBuilder().build()
 }
