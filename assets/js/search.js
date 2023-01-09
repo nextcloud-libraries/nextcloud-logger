0038754f2f362811d9cb7ffe23d6818a9ec9a78e
ChristophWurst <ChristophWurst@users.noreply.github.com>
2023-01-09 18:57:45 +0000

    Deploying to gh-pages from @ nextcloud/nextcloud-logger@82779fa9febe43c3932757ffc49c5566d5590991 🚀

--- assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"getLoggerBuilder","url":"globals.html#getloggerbuilder","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"getLogger","url":"globals.html#getlogger","classes":"tsd-kind-function"}]};
