ae92b45ecbae01baa50a52c0e0f102ebabd18be2
Ferdinand Thiessen <opensource@fthiessen.de>
2023-07-07 03:14:44 +0200

    fix: Lint files using ESLint and add CI workflow
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  clearMocks: true,
-  preset: 'ts-jest',
-  collectCoverageFrom: ['lib/**/*.ts'],
-  testEnvironment: 'jsdom',
+	clearMocks: true,
+	preset: 'ts-jest',
+	collectCoverageFrom: ['lib/**/*.ts'],
+	testEnvironment: 'jsdom',
 }
--- lib/ConsoleLogger.ts
+++ lib/ConsoleLogger.ts
@@ -1,79 +1,89 @@
-import { ILogger, LogLevel } from './contracts'
+import { IContext, ILogger, LogLevel } from './contracts'
 
 export class ConsoleLogger implements ILogger {
 
-    private context: any
+	private context: IContext
 
-    constructor(context: any) {
-        this.context = context || {}
-    }
+	constructor(context?: IContext) {
+		this.context = context || {}
+	}
 
-    private formatMessage(message: string|Error, level: LogLevel, context: any): string {
-        let msg = '[' + LogLevel[level].toUpperCase() + ']'
+	private formatMessage(message: string|Error, level: LogLevel, context?: IContext): string {
+		let msg = '[' + LogLevel[level].toUpperCase() + ']'
 
-        if (context && context.app) {
-            msg += ' ' + context.app + ': '
-        }
+		if (context && context.app) {
+			msg += ' ' + context.app + ': '
+		}
 
-        if (typeof message === 'string') return msg + message
+		if (typeof message === 'string') return msg + message
 
-        // basic error formatting
-        msg += `Unexpected ${message.name}`
-        if (message.message) msg += ` "${message.message}"`
-        // only add stack trace when debugging
-        if (level === LogLevel.Debug && message.stack) msg += `\n\nStack trace:\n${message.stack}`
+		// basic error formatting
+		msg += `Unexpected ${message.name}`
+		if (message.message) msg += ` "${message.message}"`
+		// only add stack trace when debugging
+		if (level === LogLevel.Debug && message.stack) msg += `\n\nStack trace:\n${message.stack}`
 
-        return msg
-    }
+		return msg
+	}
 
-    log(level: LogLevel, message: string|Error, context: object) {
-        if (level < this.context?.level) return;
+	log(level: LogLevel, message: string|Error, context: IContext) {
+		// Skip if level is configured and this is below the level
+		if (typeof this.context?.level === 'number' && level < this.context?.level) {
+			return
+		}
 
-        // Add error object to context
-        if (typeof message === 'object' && (context as any).error === undefined) (context as any).error = message
+		// Add error object to context
+		if (typeof message === 'object' && context?.error === undefined) {
+			context.error = message
+		}
 
-        switch (level) {
-            case LogLevel.Debug:
-                console.debug(this.formatMessage(message, LogLevel.Debug, context), context)
-                break
-            case LogLevel.Info:
-                console.info(this.formatMessage(message, LogLevel.Info, context), context)
-                break
-            case LogLevel.Warn:
-                console.warn(this.formatMessage(message, LogLevel.Warn, context), context)
-                break
-            case LogLevel.Error:
-                console.error(this.formatMessage(message, LogLevel.Error, context), context)
-                break
-            case LogLevel.Fatal:
-            default:
-                console.error(this.formatMessage(message, LogLevel.Fatal, context), context)
-                break
-        }
-    }
+		switch (level) {
+		case LogLevel.Debug:
+			console.debug(this.formatMessage(message, LogLevel.Debug, context), context)
+			break
+		case LogLevel.Info:
+			console.info(this.formatMessage(message, LogLevel.Info, context), context)
+			break
+		case LogLevel.Warn:
+			console.warn(this.formatMessage(message, LogLevel.Warn, context), context)
+			break
+		case LogLevel.Error:
+			console.error(this.formatMessage(message, LogLevel.Error, context), context)
+			break
+		case LogLevel.Fatal:
+		default:
+			console.error(this.formatMessage(message, LogLevel.Fatal, context), context)
+			break
+		}
+	}
 
-    debug(message: string|Error, context?: object): void {
-        this.log(LogLevel.Debug, message, Object.assign({}, this.context, context))
-    }
+	debug(message: string|Error, context?: IContext): void {
+		this.log(LogLevel.Debug, message, Object.assign({}, this.context, context))
+	}
 
-    info(message: string|Error, context?: object): void {
-        this.log(LogLevel.Info, message, Object.assign({}, this.context, context))
-    }
+	info(message: string|Error, context?: IContext): void {
+		this.log(LogLevel.Info, message, Object.assign({}, this.context, context))
+	}
 
-    warn(message: string|Error, context?: object): void {
-        this.log(LogLevel.Warn, message, Object.assign({}, this.context, context))
-    }
+	warn(message: string|Error, context?: IContext): void {
+		this.log(LogLevel.Warn, message, Object.assign({}, this.context, context))
+	}
 
-    error(message: string|Error, context?: object): void {
-        this.log(LogLevel.Error, message, Object.assign({}, this.context, context))
-    }
+	error(message: string|Error, context?: IContext): void {
+		this.log(LogLevel.Error, message, Object.assign({}, this.context, context))
+	}
 
-    fatal(message: string|Error, context?: object): void {
-        this.log(LogLevel.Fatal, message, Object.assign({}, this.context, context))
-    }
+	fatal(message: string|Error, context?: IContext): void {
+		this.log(LogLevel.Fatal, message, Object.assign({}, this.context, context))
+	}
 
 }
 
-export function buildConsoleLogger(context: any): ILogger {
-    return new ConsoleLogger(context)
+/**
+ * Create a new console logger
+ *
+ * @param context Optional global context which should be included for all logging messages
+ */
+export function buildConsoleLogger(context?: IContext): ILogger {
+	return new ConsoleLogger(context)
 }
