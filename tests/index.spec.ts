189cdffc0dfe02702c9fdcbc3bdf9aeae2370961
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-24 16:28:15 +0200

    feat: Migrate to vitest for testing
    This allows much better integration with ESM packages and should also be faster.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,17 +1,18 @@
+import { expect, test } from 'vitest'
 import { LogLevel, getLogger, getLoggerBuilder } from '../lib/index'
 import { ConsoleLogger } from '../lib/ConsoleLogger'
 import { LoggerBuilder } from '../lib/LoggerBuilder'
 
 test('getLoggerBuilder', () => {
-    const builder = getLoggerBuilder()
-    expect(builder).toBeInstanceOf(LoggerBuilder)
+	const builder = getLoggerBuilder()
+	expect(builder).toBeInstanceOf(LoggerBuilder)
 })
 
 test('getLogger', () => {
-    const logger = getLogger()
-    expect(logger).toBeInstanceOf(ConsoleLogger)
+	const logger = getLogger()
+	expect(logger).toBeInstanceOf(ConsoleLogger)
 })
 
 test('exported LogLevel', () => {
-    expect(typeof LogLevel === 'object').toBe(true)
+	expect(typeof LogLevel === 'object').toBe(true)
 })
